a8948a522ca3964c9e3373fd73113058f242c473
nayeli velasco <nayeserratvl@gmail.com>
2025-04-28 13:07:16 -0600

    Creacion del alumnoModel

--- src/main/java/com/UNSIJ/INESIS_BACKEND/model/AlumnoModel.java
+++ src/main/java/com/UNSIJ/INESIS_BACKEND/model/AlumnoModel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -18,4 +20,25 @@ public class AlumnoModel {
     @Column(name = "id_alumno")
     private Long id;
 
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "curp")
+    private String curp;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "matricula")
+    private String matricula;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cat_carrera", referencedColumnName = "id_cat_carrera")
+    private CatCarreraModel carrera;
 }
