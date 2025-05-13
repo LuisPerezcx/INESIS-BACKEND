1443a634740eae83946dbe3634f6ef3cef23f679
= <24mdavid25@gmail.com>
2025-05-12 23:01:59 -0600

    Se creo los repository de los modelos de MiFamilia

---
+++ src/main/java/com/UNSIJ/INESIS_BACKEND/repository/repositoryFamilia/BienesHogarRepository.java
@@ -0,0 +1,12 @@
+package com.UNSIJ.INESIS_BACKEND.repository.repositoryFamilia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.UNSIJ.INESIS_BACKEND.model.modelMiFamilia.BienesHogarModel;
+
+public interface BienesHogarRepository extends JpaRepository<BienesHogarModel, Long> {
+    // Custom query methods can be defined here if needed
+    // For example, to find a BienesHogar by its ID:
+    // Optional<BienesHogarModel> findById(Long id);
+    
+    // You can also define methods for other operations as needed
+    
+}
