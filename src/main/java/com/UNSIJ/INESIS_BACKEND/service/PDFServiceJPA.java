5a71aefade63ae1926e6f71db9c562f4b24001ca
yenis <yenisleydimartinez13@gmail.com>
2025-05-15 19:05:24 -0600

    pdf

---
+++ src/main/java/com/UNSIJ/INESIS_BACKEND/service/PDFServiceJPA.java
@@ -0,0 +1,10 @@
+package com.UNSIJ.INESIS_BACKEND.service;
+
+import com.UNSIJ.INESIS_BACKEND.utils.PDF;
+
+public class PDFServiceJPA {
+
+    public void generarPdf(){
+        String campoNombre = PDF.estudioSocioEconomico.nombreAlumno;
+    }
+}
---
+++ src/main/java/com/UNSIJ/INESIS_BACKEND/utils/PDF.java
@@ -0,0 +1,9 @@
+package com.UNSIJ.INESIS_BACKEND.utils;
+
+
+public class PDF {
+    public static class estudioSocioEconomico {
+        public static  String nombreAlumno = "nombreAlumno" ;
+
+    }
+}
