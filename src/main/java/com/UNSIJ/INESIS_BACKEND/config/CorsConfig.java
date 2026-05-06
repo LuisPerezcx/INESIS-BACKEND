8ada7187eec3426781720d606cb4880b8891ef69
LuisPerezcx <luisperez90377@gmail.com>
2026-05-06 11:19:43 -0600

    correccion de ip

--- src/main/java/com/UNSIJ/INESIS_BACKEND/config/CorsConfig.java
+++ src/main/java/com/UNSIJ/INESIS_BACKEND/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // URL del frontend
+                        .allowedOrigins("http://172.25.3.68/", "http://172.25.3.68:3000") // URL del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*");
             }
